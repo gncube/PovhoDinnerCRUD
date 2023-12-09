67c3646407802ada9d79ae990293a4a6590cfc97
Gerald Ncube <gerald.ncube@se.com>
2023-12-09 17:55:41 +0000

    Add MenusController, CreateMenuRequest, and MenuResponse classes

---
+++ src/Infrastructure/Persistence/Repositories/MenuRepository.cs
@@ -0,0 +1,12 @@
+﻿using Application.Common.Interfaces.Persistence;
+using Domain.Menus;
+
+namespace Infrastructure.Persistence.Repositories;
+public class MenuRepository : IMenuRepository
+{
+    private static readonly List<Menu> __menuList = new();
+    public void Add(Menu menu)
+    {
+        __menuList.Add(menu);
+    }
+}
