cce182a8a860c0798fa87b0e46ddcf599037fc1c
Gerald Ncube <gerald.ncube@se.com>
2023-12-09 12:47:33 +0000

    Add PovhoDinnerDbContext and PovhoDinnerDbContextFactory classes

---
+++ src/Infrastructure/Persistence/PovhoDinnerDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Users;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Persistence;
+public class PovhoDinnerDbContext : DbContext
+{
+    public PovhoDinnerDbContext(DbContextOptions<PovhoDinnerDbContext> options) : base(options) { }
+
+    public DbSet<User> Users { get; set; } = null!;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(PovhoDinnerDbContext).Assembly);
+
+        base.OnModelCreating(modelBuilder);
+    }
+}
---
+++ src/Infrastructure/Persistence/PovhoDinnerDbContextFactory.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Infrastructure.Persistence;
+public class PovhoDinnerDbContextFactory : IDesignTimeDbContextFactory<PovhoDinnerDbContext>
+{
+    public PovhoDinnerDbContext CreateDbContext(string[] args)
+    {
+        var home = Environment.GetEnvironmentVariable("HOME") ?? "";
+        var databasePath = Path.Combine(home, "PovhoDinnerDb.sqlite");
+
+        var optionsBuilder = new DbContextOptionsBuilder<PovhoDinnerDbContext>();
+        //var connectionString = "Server=localhost;Database=PovhoDinnerDb;User Id=sa;Password=admin1234*";
+        //optionsBuilder.UseSqlServer(connectionString);
+        optionsBuilder.UseSqlite($"Data Source={databasePath}");
+
+        return new PovhoDinnerDbContext(optionsBuilder.Options);
+    }
+}
