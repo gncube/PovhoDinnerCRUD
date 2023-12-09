fc3b0c01921aae25fbb8298972263c69c8f7607a
Gerald Ncube <gerald.ncube@se.com>
2023-12-09 09:05:33 +0000

    Remove authentication service and related files

---
+++ src/Application/Services/Authentication/Queries/AuthenticationQueryService.cs
@@ -0,0 +1,35 @@
+﻿using Application.Common.Interfaces;
+using Application.Services.Authentication.Common;
+
+namespace Application.Services.Authentication;
+
+public class AuthenticationQueryService : IAuthenticationQueryService
+{
+    private readonly IJwtTokenGenerator _jwtTokenGenerator;
+    private readonly IUserRepository _userRepository;
+
+    public AuthenticationQueryService(IJwtTokenGenerator jwtTokenGenerator, IUserRepository userRepository)
+    {
+        _jwtTokenGenerator = jwtTokenGenerator;
+        _userRepository = userRepository;
+    }
+
+    public AuthenticationResult Login(string email, string password)
+    {
+        // Validate password
+        var user = _userRepository.GetUserByEmail(email);
+        if (user == null)
+            throw new Exception("Invalid email or password.");
+
+        if (user.Password != password)
+            throw new Exception("Invalid email or password.");
+
+        // Create Jwt Token
+        string token = _jwtTokenGenerator.GenerateToken(user);
+
+
+        return new AuthenticationResult(
+                user,
+                token);
+    }
+}
---
+++ src/Application/Services/Authentication/Queries/IAuthenticationQueryService.cs
@@ -0,0 +1,7 @@
+﻿using Application.Services.Authentication.Common;
+
+namespace Application.Services.Authentication;
+public interface IAuthenticationQueryService
+{
+    AuthenticationResult Login(string email, string password);
+}
