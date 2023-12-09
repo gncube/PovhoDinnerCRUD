bcefb786ad6cf8f6ed9705ae548b7455690f5e17
Gerald Ncube <gerald.ncube@se.com>
2023-12-09 12:47:24 +0000

    Add UserRepository implementation

---
+++ src/Infrastructure/Persistence/Repositories/UserRepository.cs
@@ -0,0 +1,22 @@
+﻿using Application.Common.Interfaces.Persistence;
+using Domain.Users;
+
+namespace Infrastructure.Persistence.Repositories;
+
+public class UserRepository : IUserRepository
+{
+    public UserRepository()
+    {
+
+    }
+    private static readonly List<User> _users = new();
+    public User? GetUserByEmail(string email)
+    {
+        return _users.FirstOrDefault(u => u.Email == email);
+    }
+
+    public void Add(User user)
+    {
+        _users.Add(user);
+    }
+}
