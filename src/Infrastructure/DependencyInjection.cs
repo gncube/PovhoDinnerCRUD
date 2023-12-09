194e7ae84c4ba077ebcf9f3f3f1768916f00ff17
Gerald Ncube <gerald.ncube@se.com>
2023-12-09 00:41:32 +0000

    Add dependencies and services for JWT token generation and date/time handling

--- src/Infrastructure/DependencyInjection.cs
+++ src/Infrastructure/DependencyInjection.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Application.Common.Interfaces;
+using Infrastructure.Authentication;
+using Infrastructure.Services;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Infrastructure;
@@ -7,6 +10,9 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
     {
+        services.Configure<JwtSettings>(configuration.GetSection(JwtSettings.SectionName));
+        services.AddScoped<IJwtTokenGenerator, JwtTokenGenerator>();
+        services.AddScoped<IDateTimeProvider, DateTimeProvider>();
         return services;
     }
 }
