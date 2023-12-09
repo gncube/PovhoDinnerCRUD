e45b409faab913dbefa9bfcdd698163ea72ac05f
Gerald Ncube <gerald.ncube@se.com>
2023-12-09 11:28:08 +0000

    Add DependencyInjection class and update Program.cs

---
+++ src/Api/DependencyInjection.cs
@@ -0,0 +1,16 @@
+﻿using Api.Common.Errors;
+using Api.Common.Mapping;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+
+namespace Api;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddPresentation(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddControllers();
+        services.AddSingleton<ProblemDetailsFactory, CustomProblemDetailsFactory>();
+        services.AddCommonMapping();
+        return services;
+    }
+}
--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -1,23 +1,23 @@
-using Api.Common.Errors;
+using Api;
 using Application;
 using Infrastructure;
-using Microsoft.AspNetCore.Mvc.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 {
-    builder.Services.AddApplication();
-    builder.Services.AddInfrastructure(builder.Configuration);
-
-    builder.Services.AddControllers();
-
-    builder.Services.AddSingleton<ProblemDetailsFactory, CustomProblemDetailsFactory>();
+    builder.Services
+        .AddPresentation(builder.Configuration)
+        .AddApplication()
+        .AddInfrastructure(builder.Configuration);
 }
 
 
 var app = builder.Build();
 {
     app.UseExceptionHandler("/error");
+
     app.UseHttpsRedirection();
+    app.UseAuthentication();
+    app.UseAuthorization();
     app.MapControllers();
     app.Run();
 }
