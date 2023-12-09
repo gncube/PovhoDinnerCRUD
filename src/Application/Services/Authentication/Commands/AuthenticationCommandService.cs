f966394bb23be84c672a9b7d1eb4da1becbb4959
Gerald Ncube <gerald.ncube@se.com>
2023-12-09 09:05:24 +0000

    Add authentication command service and interface

---
+++ src/Application/Services/Authentication/Commands/AuthenticationCommandService.cs
@@ -0,0 +1,42 @@
+﻿using Application.Common.Interfaces;
+using Application.Services.Authentication.Common;
+using Domain.Users;
+using Domain.Users.ValueObjects;
+
+namespace Application.Services.Authentication.Commands;
+
+public class AuthenticationCommandService : IAuthenticationCommandService
+{
+    private readonly IJwtTokenGenerator _jwtTokenGenerator;
+    private readonly IUserRepository _userRepository;
+
+    public AuthenticationCommandService(IJwtTokenGenerator jwtTokenGenerator, IUserRepository userRepository)
+    {
+        _jwtTokenGenerator = jwtTokenGenerator;
+        _userRepository = userRepository;
+    }
+    public AuthenticationResult Register(string firstName, string lastName, string email, string password)
+    {
+        // Check if email is already registered
+        if (_userRepository.GetUserByEmail(email) != null)
+            throw new Exception("Email already registered.");
+
+        var user = new User
+        {
+            Id = new UserId(Guid.NewGuid()),
+            FirstName = firstName,
+            LastName = lastName,
+            Email = email,
+            Password = password
+        };
+
+        _userRepository.Add(user);
+
+        // Create Jwt Token
+        string token = _jwtTokenGenerator.GenerateToken(user);
+
+        return new AuthenticationResult(
+            user,
+            token);
+    }
+}
---
+++ src/Application/Services/Authentication/Commands/IAuthenticationCommandService.cs
@@ -0,0 +1,7 @@
+﻿using Application.Services.Authentication.Common;
+
+namespace Application.Services.Authentication.Commands;
+public interface IAuthenticationCommandService
+{
+    AuthenticationResult Register(string firstName, string lastName, string email, string password);
+}
